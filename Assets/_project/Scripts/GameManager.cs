e8ec1fd15287b4442fc947214292c6d375bba378
ssoprych <sebastian.soprych@gmail.com>
2023-01-03 23:23:29 +0100

    fixed speed bug

--- Assets/_project/Scripts/GameManager.cs
+++ Assets/_project/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Movement._speed = 2;
         Time.timeScale = 1;
     }
 
